8323d67c8395f621db75e30d4eb3c302d340ca9b
jerome ullmann <jerome.ullmann@gmail.com>
2016-01-20 16:58:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ju/mainscreenactivity/MainScreenActivity.java
@@ -0,0 +1,58 @@
+package com.example.ju.mainscreenactivity;
+
+/**
+ * Created by pevd620 on 29/12/2015.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+
+
+public class MainScreenActivity extends Activity {
+
+    Button btnViewProducts;
+    Button btnNewProduct;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+
+
+        // Buttons
+        btnViewProducts = (Button) findViewById(R.id.btnViewProducts);
+        btnNewProduct = (Button) findViewById(R.id.btnCreateProduct);
+
+        // view products click event
+        btnViewProducts.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Launching All products Activity
+                Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        // view products click event
+        btnNewProduct.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Launching create new product activity
+                Intent i = new Intent(getApplicationContext(), NewProductActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+}
+
